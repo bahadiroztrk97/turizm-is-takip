515d0741f71e97e6441265d0f070d81bc5ae2b05
bahadiroztrk97 <bahadirozturk097@gmail.com>
2026-06-13 14:52:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Merhaba Turizm Otomasyonu")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = """
+🎉 Turizm İş Takip Sistemi Aktif
+
+GitHub Actions başarılı şekilde çalışıyor.
+
+Bir sonraki aşamada gerçek iş ilanlarını taramaya başlayacağız.
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print(response.text)
